4f6698880815aba6723c505fec7768b236482414
Adrien Lo <adrien.lo@gmail.com>
2014-05-14 09:13:10 -0700

    updated gitignore

---
+++ server.js
@@ -0,0 +1,11 @@
+var http 		= require('http'),
+	express 	= require('express'),
+	ecstatic 	= require('ecstatic'),
+	app 		= express(),
+	ip			= "127.0.01",
+	port		= "8081";
+
+app.use(ecstatic({ root: __dirname }));
+http.createServer(app).listen(port);
+
+console.log('Server running at http://127.0.0.1:' + port);
